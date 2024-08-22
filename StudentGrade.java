9d3859a4eba52783ea190496dd331cbc3a0d56aa
Yaduvansh Mani <yaduvanshmani558@gmail.com>
2024-08-22 13:07:27 +0530

    Add files via upload

---
+++ StudentGrade.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class StudentGrade {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        
+        double[] marks = new double[numSubjects];
+        double totalMarks = 0;
+        
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + " (out of 100): ");
+            marks[i] = scanner.nextDouble();
+            totalMarks += marks[i];
+        }
+        
+        double averagePercentage = totalMarks / numSubjects;
+        String grade = calculateGrade(averagePercentage);
+        
+        System.out.println("\nResults:");
+        System.out.printf("Total Marks: %.2f\n", totalMarks);
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+        System.out.println("Grade: " + grade);
+        
+        scanner.close();
+    }
+    
+    private static String calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return "A+";
+        } else if (averagePercentage >= 80) {
+            return "A";
+        } else if (averagePercentage >= 70) {
+            return "B";
+        } else if (averagePercentage >= 60) {
+            return "C";
+        } else if (averagePercentage >= 50) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
